fd51d877302ea3d6d3b2017560d32b546d4e7aac
Aytac <111279845+Seniorita97@users.noreply.github.com>
2024-05-15 08:11:47 +0400

    MSV Task1

---
+++ WebApplication2/Controllers/UserController1.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class UserController1 : Controller
+    {
+        // GET: UserController1
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Aygun";
+            ViewBag.Surname = "Kazimova";
+            ViewBag.Age = "55";
+            ViewBag.Country = "Azerbaijan";
+            ViewBag.PhoneNumber = "0555555555";
+
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        // GET: UserController1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UserController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserController1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UserController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UserController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
